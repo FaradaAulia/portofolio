c9735fe6f4e96552793691772761d83eb64e8a2c
Farada Aulia <farada.240170214@mhs.unimal.ac.id>
2026-05-25 19:35:09 +0700

    fix: modifikasi title dan pastikan cdn tailwind aktif

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $profile['name'] }} | {{ $profile['title'] }}</title>
+    <title>{{ $profile['name'] }} | {{ $profile['title'] }} !</title>
     
     <meta name="description" content="{{ $profile['sub_title'] }}. {{ $profile['bio_short'] }}">
     <meta name="keywords" content="Portfolio, Full Stack Developer, Laravel Developer, Web Developer Indonesia, {{ $profile['name'] }}">
