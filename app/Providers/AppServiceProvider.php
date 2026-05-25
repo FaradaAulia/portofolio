8445b3f2425ddb3ad9a292b6d6a5577b13445849
Farada Aulia <farada.240170214@mhs.unimal.ac.id>
2026-05-25 19:24:40 +0700

    fix: memaksa url menggunakan https agar css ter-load di vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- 1. WAJIB TAMBAH BARIS INI
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. TAMBAH KONDISI INI: Jika berjalan di server (production), paksa pakai HTTPS
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
